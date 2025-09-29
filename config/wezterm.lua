2c44ba2dbc319c8220b76ef583e9cdcc3db3aa2b
lizelit <lizelit.you@gmail.com>
2025-09-30 03:41:53 +0900

    lua-wezterm

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -2,11 +2,11 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Everforest Dark Medium (Gogh)',  -- ここを好きなバリエーションに
-config.font = wezterm.font("JetBrainsMono Nerd Font"),
-config.font_size = 14,
-config.hide_tab_bar_if_only_one_tab = true,
-config.window_background_opacity = 0.9,
-config.macos_window_background_blur = 20,
+config.color_scheme = 'Everforest Dark Medium (Gogh)'
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 14
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
 
 return config
