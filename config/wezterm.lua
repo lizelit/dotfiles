46bb69a27d438abd4df16ab8c84f905da8a38c05
lizelit <lizelit.you@gmail.com>
2025-09-30 03:34:41 +0900

    lua-wezterm

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,13 +1,7 @@
 local wezterm = require 'wezterm'
 
-local wezterm = require 'wezterm'
-
-for name, _ in pairs(wezterm.get_builtin_color_schemes()) do
-  wezterm.log_info("Color scheme available: " .. name)
-end
-
 return {
-  color_scheme = "Everforest Dark (Medium)",  -- ここを好きなバリエーションに
+  color_scheme = "Everforest Dark Medium (Gogh)",  -- ここを好きなバリエーションに
   font = wezterm.font("JetBrainsMono Nerd Font"),
   font_size = 14.0,
   hide_tab_bar_if_only_one_tab = true,
