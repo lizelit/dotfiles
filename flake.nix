9f5dc6004e93b58acf98492be3adaf36f5fc0300
lizelit <lizelit.you@gmail.com>
2025-10-02 09:55:22 +0900

    nixvim

--- flake.nix
+++ flake.nix
@@ -1,67 +1,39 @@
 {
-  description = "My personal Darwin system configuration";
+  description = "Darwin system with NixVim and Home Manager";
 
   inputs = {
-    # 1. Nixpkgs: すべてのパッケージの源
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    
-    # 2. Nix-Darwin: macOSシステム設定の管理
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
-    # 3. Home Manager: ユーザー環境の設定を管理
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05"; # バージョン指定を推奨
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
-    # 4. NixVim: Neovimの設定モジュール
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager, nixvim, ... }: 
-  let
-    # 🍎 あなたのシステムアーキテクチャに合わせて設定
-    system = "aarch64-darwin"; 
-    
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-    
-    username = "lizelit";
-    hostname = "Mac-2";
-    homeDirectory = "/Users/${username}";
-  in
-  {
-    # 🌟 Darwin system configuration
-    darwinConfigurations."${hostname}" = nix-darwin.lib.darwinSystem {
-      inherit system;
+  outputs = { self, nixpkgs, nix-darwin, home-manager, nixvim, ... }: {
+    darwinConfigurations."Mac-2" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
       modules = [
         ./darwin.nix
-        home-manager.darwinModules.home-manager # nix-darwin経由でHome Managerをインポート
+        home-manager.darwinModules.home-manager
         {
-          # Home Managerの設定ブロック
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          
-          # ユーザーの設定ファイル
-          home-manager.users.${username} = {
-            imports = [
-              nixvim.homeModules.nixvim
-              ./home.nix
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.lizelit = import ./home.nix;
+            sharedModules = [
+              nixvim.homeManagerModules.nixvim
             ];
           };
         }
       ];
-      specialArgs = { 
-        inherit inputs username hostname homeDirectory pkgs; 
-      };
     };
   };
 }
--- home.nix
+++ home.nix
@@ -1,123 +1,312 @@
-{ config, pkgs, inputs, username ? "lizelit", homeDirectory ? "/Users/lizelit", ... }:
+{ config, pkgs, ... }:
 
 {
-  # Home Manager basic configuration
-  home = {
-    inherit username homeDirectory;
-    stateVersion = "24.05";
+  home.username = "lizelit";
+  home.homeDirectory = "/Users/lizelit";
+  home.stateVersion = "24.05";
 
-    sessionVariables = {
-      EDITOR = "nvim";
-      BROWSER = "Safari";
-      TERMINAL = "wezterm";
-    };
+  # 基本パッケージ
+  home.packages = with pkgs; [
+    ripgrep
+    fd
+    git
+    gcc
+    nodejs
+    tree
+    wget
+    curl
+  ];
 
-    packages = with pkgs; [
-      # CLI tools
-      bat              # cat alternative
-      eza              # ls alternative
-      fd               # find alternative
-      fzf              # fuzzy finder
-      ripgrep          # grep alternative
-      jq               # JSON processor
-      yq               # YAML processor
-      tree             # directory tree
-      htop             # process monitor
-      
-      # Development tools
-      lazygit
-      gh               # GitHub CLI
-      
-      # Language servers
-      nil              # Nix LSP
-      rust-analyzer
-      nodePackages.typescript-language-server
-
-      mas
-      
-      # Other tools
-      ffmpeg
-      imagemagick
-      pandoc
-
-      pkgs.nerd-fonts.jetbrains-mono
-    ];
-  };
-
+  # NixVim設定
   programs.nixvim = {
     enable = true;
-    
-    # 💡 基本的なエディタオプション
-    options = {
-      relativenumber = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+
+    # カラースキーム
+    colorschemes.tokyonight = {
+      enable = true;
+      settings.style = "night";
+    };
+
+    # オプション
+    opts = {
       number = true;
+      relativenumber = true;
       expandtab = true;
-      tabstop = 2;
       shiftwidth = 2;
-      # 行折り返しをオフ
-      wrap = false; 
+      tabstop = 2;
+      smartindent = true;
+      ignorecase = true;
+      smartcase = true;
+      termguicolors = true;
+      signcolumn = "yes";
+      cursorline = true;
+      wrap = false;
+      splitright = true;
+      splitbelow = true;
+      mouse = "a";
+      clipboard = "unnamedplus";
+      undofile = true;
+      updatetime = 250;
+      timeoutlen = 300;
+      scrolloff = 8;
+      sidescrolloff = 8;
     };
 
-    # 🌈 カラースキームと見た目
-    colorScheme = "catppuccin";
-    colorschemes = {
-      catppuccin = {
-        enable = true;
-        settings.flavour = "mocha";
-      };
+    globals = {
+      mapleader = " ";
+      maplocalleader = "\\";
     };
 
-    # 🌟 LazyVim風のモダンな機能の有効化
+    # キーマップ
+    keymaps = [
+      { mode = "n"; key = "j"; action = "v:count == 0 ? 'gj' : 'j'"; options = { expr = true; silent = true; }; }
+      { mode = "n"; key = "k"; action = "v:count == 0 ? 'gk' : 'k'"; options = { expr = true; silent = true; }; }
+      { mode = "n"; key = "<C-h>"; action = "<C-w>h"; options = { desc = "Go to left window"; }; }
+      { mode = "n"; key = "<C-j>"; action = "<C-w>j"; options = { desc = "Go to lower window"; }; }
+      { mode = "n"; key = "<C-k>"; action = "<C-w>k"; options = { desc = "Go to upper window"; }; }
+      { mode = "n"; key = "<C-l>"; action = "<C-w>l"; options = { desc = "Go to right window"; }; }
+      { mode = "n"; key = "<C-Up>"; action = ":resize +2<CR>"; }
+      { mode = "n"; key = "<C-Down>"; action = ":resize -2<CR>"; }
+      { mode = "n"; key = "<C-Left>"; action = ":vertical resize -2<CR>"; }
+      { mode = "n"; key = "<C-Right>"; action = ":vertical resize +2<CR>"; }
+      { mode = "n"; key = "<A-j>"; action = ":m .+1<CR>=="; }
+      { mode = "n"; key = "<A-k>"; action = ":m .-2<CR>=="; }
+      { mode = "v"; key = "<A-j>"; action = ":m '>+1<CR>gv=gv"; }
+      { mode = "v"; key = "<A-k>"; action = ":m '<-2<CR>gv=gv"; }
+      { mode = "n"; key = "<S-h>"; action = ":bprevious<CR>"; options = { desc = "Prev buffer"; }; }
+      { mode = "n"; key = "<S-l>"; action = ":bnext<CR>"; options = { desc = "Next buffer"; }; }
+      { mode = "n"; key = "[b"; action = ":bprevious<CR>"; }
+      { mode = "n"; key = "]b"; action = ":bnext<CR>"; }
+      { mode = "n"; key = "<Esc>"; action = ":noh<CR><Esc>"; }
+      { mode = "n"; key = "<C-s>"; action = ":w<CR>"; }
+      { mode = "v"; key = "<"; action = "<gv"; }
+      { mode = "v"; key = ">"; action = ">gv"; }
+      { mode = "n"; key = "<leader>e"; action = ":Neotree toggle<CR>"; options = { desc = "Toggle file explorer"; }; }
+      { mode = "n"; key = "<leader>E"; action = ":Neotree reveal<CR>"; options = { desc = "Reveal in file explorer"; }; }
+    ];
+
+    # プラグイン
     plugins = {
-      # 起動高速化のためのプラグインローダー
-      lazy.enable = true;
-      
-      # LSPクライアントの有効化
-      lsp.enable = true;
-      
-      # 補完エンジン
-      cmp.enable = true;
-      
-      # 構文解析とハイライト
+      # LSP
+      lsp = {
+        enable = true;
+        keymaps = {
+          silent = true;
+          diagnostic = {
+            "[d" = "goto_prev";
+            "]d" = "goto_next";
+            "<leader>cd" = "open_float";
+          };
+          lspBuf = {
+            "gd" = "definition";
+            "gD" = "declaration";
+            "gi" = "implementation";
+            "gy" = "type_definition";
+            "gr" = "references";
+            "K" = "hover";
+            "gK" = "signature_help";
+            "<leader>ca" = "code_action";
+            "<leader>cr" = "rename";
+            "<leader>cf" = "format";
+          };
+        };
+        servers = {
+          nil_ls.enable = true;
+        };
+      };
+
+      # 補完
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+        settings = {
+          snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+          mapping = {
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-e>" = "cmp.mapping.abort()";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<Tab>" = ''
+              cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif require("luasnip").expand_or_jumpable() then
+                  require("luasnip").expand_or_jump()
+                else
+                  fallback()
+                end
+              end, { "i", "s" })
+            '';
+            "<S-Tab>" = ''
+              cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif require("luasnip").jumpable(-1) then
+                  require("luasnip").jump(-1)
+                else
+                  fallback()
+                end
+              end, { "i", "s" })
+            '';
+          };
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "luasnip"; }
+            { name = "buffer"; }
+            { name = "path"; }
+          ];
+        };
+      };
+
+      luasnip.enable = true;
+      cmp-nvim-lsp.enable = true;
+      cmp-buffer.enable = true;
+      cmp-path.enable = true;
+      cmp_luasnip.enable = true;
+
+      # UI
+      lualine = {
+        enable = true;
+        settings = {
+          options = {
+            theme = "tokyonight";
+            globalstatus = true;
+          };
+        };
+      };
+
+      bufferline = {
+        enable = true;
+        settings = {
+          options = {
+            diagnostics = "nvim_lsp";
+            always_show_bufferline = false;
+          };
+        };
+      };
+
+      web-devicons.enable = true;
+      notify = {
+        enable = true;
+        timeout = 3000;
+      };
+      dressing.enable = true;
+
+      indent-blankline = {
+        enable = true;
+        settings = {
+          indent.char = "│";
+          scope.enabled = false;
+        };
+      };
+
+      which-key = {
+        enable = true;
+        settings = {
+          spec = [
+            { __unkeyed-1 = "<leader>b"; group = "buffer"; }
+            { __unkeyed-1 = "<leader>c"; group = "code"; }
+            { __unkeyed-1 = "<leader>f"; group = "file/find"; }
+            { __unkeyed-1 = "<leader>g"; group = "git"; }
+            { __unkeyed-1 = "<leader>w"; group = "windows"; }
+          ];
+        };
+      };
+
+      alpha = {
+        enable = true;
+        layout = [
+          { type = "padding"; val = 2; }
+          {
+            type = "text";
+            val = [
+              "███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗"
+              "████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║"
+              "██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║"
+              "██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║"
+              "██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║"
+              "╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝"
+            ];
+            opts = { position = "center"; hl = "Type"; };
+          }
+        ];
+      };
+
+      # エディター
+      neo-tree = {
+        enable = true;
+        window = {
+          position = "left";
+          width = 30;
+        };
+      };
+
+      telescope = {
+        enable = true;
+        keymaps = {
+          "<leader><space>" = { action = "find_files"; };
+          "<leader>ff" = { action = "find_files"; };
+          "<leader>fg" = { action = "live_grep"; };
+          "<leader>/" = { action = "live_grep"; };
+          "<leader>fb" = { action = "buffers"; };
+          "<leader>fr" = { action = "oldfiles"; };
+        };
+        extensions.fzf-native.enable = true;
+      };
+
+      gitsigns = {
+        enable = true;
+        settings.signs = {
+          add.text = "▎";
+          change.text = "▎";
+          delete.text = "";
+          topdelete.text = "";
+          changedelete.text = "▎";
+        };
+      };
+
+      comment.enable = true;
+      nvim-autopairs.enable = true;
+      toggleterm = {
+        enable = true;
+        settings = {
+          direction = "float";
+          open_mapping = "[[<C-\\>]]";
+        };
+      };
+
+      # Treesitter
       treesitter = {
         enable = true;
-        settings.ensure_installed = [ 
-          "lua" "nix" "javascript" "typescript" "html" "css"
-        ];
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+        };
       };
-
-      # ファイル・バッファ・Gitなどのファジーファインダー
-      telescope.enable = true;
-      
-      # ファイルツリー
-      neo-tree.enable = true;
-      
-      # Git連携 (行単位の変更表示など)
-      gitsigns.enable = true;
-      
-      # ステータスライン
-      lualine.enable = true;
-      
-      # 言語サーバーのインストール (mason.nvimの代わり)
-      # 必要な言語サーバーをここで定義すれば、Nixがインストールしてくれます。
-      mason = {
-        enable = true;
-        # 例: TypeScript/JavaScriptとNixのLSPをインストール
-        ensureInstalled = [
-          "typescript-language-server"
-          "nil" # Nix Language Server
-        ];
-      };
-
     };
+
+
   };
 
-  programs.wezterm = {
+  # direnv
+  programs.direnv = {
     enable = true;
+    nix-direnv.enable = true;
+    enableZshIntegration = true;
   };
 
+  # Git
+  programs.git = {
+    enable = true;
+    userName = "lizelit";
+    userEmail = "your.email@example.com";
+  };
 
-  # Zsh with Oh My Zsh and useful plugins
+  # Zsh
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -125,68 +314,15 @@
     syntaxHighlighting.enable = true;
     
     shellAliases = {
-      # Basic aliases
-      ll = "eza -la";
-      la = "eza -la";
-      lt = "eza --tree";
-      cat = "bat";
-      grep = "rg";
-      
-      # Git aliases
-      g = "git";
-      gs = "git status";
-      ga = "git add";
-      gc = "git commit";
-      gp = "git push";
-      gl = "git pull";
-      
-      # Nix aliases
-      nr = "nix run";
-      ns = "nix shell";
-      nf = "nix flake";
-      
-      # Darwin aliases
-      dr = "sudo darwin-rebuild switch --flake ~/dotfiles";
-      hm = "home-manager switch --flake ~/dotfiles";
+      ll = "ls -la";
+      vim = "nvim";
+      rebuild = "darwin-rebuild switch --flake ~/dotfiles";
     };
     
-    history = {
-      size = 10000;
-      path = "${config.xdg.dataHome}/zsh/history";
-    };
-    
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "fzf" "docker" ];
-      theme = "robbyrussell";
-    };
+    initExtra = ''
+      export EDITOR=nvim
+    '';
   };
 
-  # Direnv for automatic environment switching
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-  };
-
-  # Fzf fuzzy finder
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-    
-    defaultOptions = [
-      "--height 40%"
-      "--layout=reverse"
-      "--border"
-      "--inline-info"
-    ];
-  };
-
-  # XDG directories
-  xdg = {
-    enable = true;
-    configHome = "${homeDirectory}/.config";
-    dataHome = "${homeDirectory}/.local/share";
-    cacheHome = "${homeDirectory}/.cache";
-    configFile."wezterm/wezterm.lua".source = ./config/wezterm.lua;
-  };
+  programs.home-manager.enable = true;
 }
