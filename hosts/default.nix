b92467bebddf0f882a5d783fa8d9b59a207cbd50
lizelit <lizelit.you@gmail.com>
2025-12-13 02:39:20 +0900

    host

---
+++ hosts/default.nix
@@ -0,0 +1,88 @@
+{ inputs, ... };
+
+let
+  inherit (inputs) nixpkgs nix-darwin home-manager;
+  username = "lizelit";
+  hostname = "TMBA-2";
+  homeDiretory = "/Users/${username}";
+
+  specialArgs = {
+    inherit inputs username hostname homeDirectory;
+  };
+in
+{
+  darwinConfiguration."${hostname}" = nix-darwin.lib.darwinSystem {
+    system = " aarch64-darwin";
+    inherit specialArgs;
+
+    modules = [
+      ../darwin
+
+      home-manager.darwinModules.home-manager
+      {
+        home-manager = {
+          backupFileExtension = "backup";
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          extraSpecialArgs = specialArgs;
+
+          users.${username} = import ../home;
+
+          sharedModules = [
+          ];
+        };
+      }
+    ];
+  };
+}
+
+
+
+# {
+#   description = "Darwin system and Home Manager";
+#   inputs = {
+#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+#     nix-darwin = {
+#       url = "github:LnL7/nix-darwin";
+#       inputs.nixpkgs.follows = "nixpkgs";
+#     };
+#     home-manager = {
+#       url = "github:nix-community/home-manager";
+#       inputs.nixpkgs.follows = "nixpkgs";
+#     };
+#   };
+#   outputs = {
+#     self,
+#     nixpkgs,
+#     nix-darwin,
+#     home-manager,
+#     ...
+#   } @ inputs: let
+#     username = "lizelit";
+#     hostname = "TMBA-2";
+#     homeDirectory = "/Users/lizelit";
+#     specialArgs = {
+#       inherit inputs username hostname homeDirectory;
+#     };
+#   in {
+#     darwinConfigurations."TMBA-2" = nix-darwin.lib.darwinSystem {
+#       system = "aarch64-darwin";
+#       inherit specialArgs;
+#       modules = [
+#         ./darwin/default.nix
+#         home-manager.darwinModules.home-manager
+#         {
+#           home-manager = {
+#             backupFileExtension = "backup";
+#             useGlobalPkgs = true;
+#             useUserPackages = true;
+#             extraSpecialArgs = specialArgs;
+#             users.lizelit = import ./home.nix;
+#             sharedModules = [
+#             ];
+#           };
+#         }
+#       ];
+#     };
+#   };
+# }
