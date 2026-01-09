cc8d4752d996672b6def4e55a8b7ba46af010304
lizelit <lizelit.you@gmail.com>
2026-01-10 05:16:17 +0900

    nice

--- home/modules/nixvim/keymaps.nix
+++ home/modules/nixvim/keymaps.nix
@@ -12,13 +12,7 @@
       {
         mode = "n";
         key = "gw";
-        action = ''
-          lua require("flash").jump({
-            search = { mode = "search", max_length = 2 },
-            label = { after = { 0, 0 } },
-            pattern = [[\<\w]]
-          })
-        '';
+        action = "<cmd>lua require('flash').jump()<CR>";
         options.desc = "Go to Word";
       }
 
@@ -26,9 +20,11 @@
       { mode = "n"; key = "<leader>/"; action = "<cmd>Telescope live_grep<CR>"; options.desc = "Grep"; }
       { mode = "n"; key = "<leader>b"; action = "<cmd>Telescope buffers<CR>"; options.desc = "Buffers"; }
       { mode = "n"; key = "<leader>e"; action = "<cmd>lua require('oil').toggle_float()<CR>"; options.desc = "Explorer"; }
-      { mode = "n"; key = "<leader>d"; action = "<cmd>Trouble diagnostics toggle<CR>"; options.desc = "Explorer"; }
+      # { mode = "n"; key = "<leader>d"; action = "<cmd>Trouble diagnostics toggle<CR>"; options.desc = "Diagnostics"; }
       { mode = "n"; key = "<leader>t"; action = "<cmd>ToggleTerm<CR>"; options.desc = "Terminal"; }
       { mode = "n"; key = "<leader>g"; action = "<cmd>LazyGit<CR>"; options.desc = "LazyGit"; }
+
+      { mode = "n"; key = "<C-c>"; action = "<cmd> lua require('Comment.api').toggle.linewise.current()<CR>"; options.desc = "Comment"; }
     ];
   };
 }
--- home/modules/nixvim/plugins/lsp.nix
+++ home/modules/nixvim/plugins/lsp.nix
@@ -7,11 +7,62 @@
     treesitter.enable = true;
     none-ls.enable = true;
     trouble.enable = true;
+    cmp-nvim-lsp.enable = true;
+    cmp-path.enable = true;
+    cmp-buffer.enable = true;
 
     lsp = {
+      diagnostics = {
+        virtual_text = false;
+        virtual_lines = {
+          only_current_line = false;
+        };
+        underline = true;
+        signs = true;
+      };
+
+      keymaps = {
+        diagnostic = {
+          "<leader>d" = {
+            action = "open_float";
+          };
+        };
+      };
+
       servers = {
         nixd.enable = true;
-        rust_analyzer.enable = true;
+        rust_analyzer = {
+          enable = true;
+          settings = {
+            inlayHints = {
+              bindingModeHints.enable = true;
+              chainingHints.enable = true;
+              closingBraceHints.enable = true;
+              parameterHints.enable = true;
+              typeHints.enable = true;
+            };
+          };
+          onAttach.function = "vim.lsp.inlay_hint.enable(true)";
+        };
+      };
+    };
+
+    cmp = {
+      settings = {
+        autoEnableSources = true;
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "path"; }
+          { name = "buffer"; }
+        ];
+        mapping = {
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<S-tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+        };
       };
     };
 
@@ -22,7 +73,7 @@
     };
 
     extraPackages = with pkgs; [
-      nixd
+      nix
       nixpkgs-fmt
     ];
   };
