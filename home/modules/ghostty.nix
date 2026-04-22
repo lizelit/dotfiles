4f166d73c14789d4fc6b41c9296da2326f0cf123
lizelit <lizelit.you@gmail.com>
2026-04-22 13:53:09 +0900

    yes

--- home/modules/ghostty.nix
+++ home/modules/ghostty.nix
@@ -1,21 +1,16 @@
-{lib, pkgs, ... }:
+{ ... }:
 
 {
-  programs.ghostty = {
-    enable = true;
-    package = pkgs.runCommand "ghostty-dummy" {} "mkdir -p $out";
-    settings = {
-      font-size = lib.mkForce 14;
-      macos-titlebar-style = "hidden";
-      window-padding-x = 8;
-      window-padding-y = 4;
-      macos-option-as-alt = true;
-      background-opacity = 0.8;
-      # quick-terminal-position = "center";
-      # quick-terminal-size = "300px";
-      # keybind = [
-      #   "global:alt+space=toggle_quick_terminal"
-      # ];
-    };
+  xdg.configFile."ghostty/config" = {
+    text = ''
+      font-family = "UDEV Gothic NFLG"
+      font-size = 14
+      macos-titlebar-style = "hidden"
+      window-padding-x = 8
+      window-padding-y = 4
+      macos-option-as-alt = true
+      background-opacity = 0.8
+      background-blur = true
+    '';
   };
 }
